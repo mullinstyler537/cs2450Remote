2d282f1318cd6761087bd3c1148108300f81555e
Tyler Mullins <mullinstyler537@gmail.com>
2025-08-29 12:08:02 -0600

    first commit

---
+++ age_guesser.py
@@ -0,0 +1,17 @@
+import random
+
+print("Hi there! I'm going to try to guess your age.")
+
+name = input("What's your name? ")
+
+guessed_correctly = False
+
+while not guessed_correctly:
+    guess = random.randint(15, 30)
+    answer = input(f"Are you {guess} years old? (y/n): ").lower()
+    
+    if answer == 'y':
+        print(f"Awesome! {name} is {guess} years old.")
+        guessed_correctly = True
+    else:
+        print("Dang it.")
